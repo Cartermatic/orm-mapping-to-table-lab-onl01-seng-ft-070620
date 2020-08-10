e58bddaf5fa2c1d23ee841b703eccc8641c188e2
Carter Parrett <cparrett94@hotmail.com>
2020-08-10 16:00:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
   
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE [IF EXISTS]
+      DROP TABLE students
   
   
   # Remember, you can access your database connection anywhere in this class
